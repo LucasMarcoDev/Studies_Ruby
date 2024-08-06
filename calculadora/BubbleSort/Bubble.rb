7a6f14668052d88c233d7ca964ee4e7d9c782ff8
Lucas Marco <lucas.paulamarco@hotmail.com.br>
2024-08-06 18:00:51 -0300

    exercicio Bubble_sort

---
+++ calculadora/BubbleSort/Bubble.rb
@@ -0,0 +1,9 @@
+bubble = [9 , 8, 0 , 5 , 1 ,3 ,2 ]
+
+bubble.each do |lista|
+  if lista (0 > 1)
+    puts lista
+
+  end
+
+end
--- calculadora/main_calculadora.rb
+++ calculadora/main_calculadora.rb
@@ -4,10 +4,10 @@ require_relative "adicao"
 require_relative "subtração"
 
 
-subtr = Subt.new(5,5)
-di = Divisao.new(20, 5)
-ad = Adic.new(10, 20)
-mult = Mult.new(10 , 10)
+subtr = Subt.new(10,5)
+di = Divisao.new(30, 2)
+ad = Adic.new(50, 20)
+mult = Mult.new(8 , 7)
 puts subtr.visu
 puts ad.soma
 puts di.show
